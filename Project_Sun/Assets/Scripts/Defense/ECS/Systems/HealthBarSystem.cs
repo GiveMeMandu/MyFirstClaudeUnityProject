825a598004d884b8f4752f81ca39959ad2afa37a
KimWoochan <lion0303@hanyang.ac.kr>
2026-03-25 11:59:58 +0900

    feat(dots-defense-system): SF-06 사망 이펙트 + 체력바 (오브젝트 풀링, 토글)

---
+++ Project_Sun/Assets/Scripts/Defense/ECS/Systems/HealthBarSystem.cs
@@ -0,0 +1,44 @@
+using Unity.Burst;
+using Unity.Entities;
+
+namespace ProjectSun.Defense.ECS
+{
+    /// <summary>
+    /// 체력바 표시 타이머를 관리하는 시스템.
+    /// 피격 시 HealthBarTimer를 2초로 설정하고, 매 프레임 감소.
+    /// MonoBehaviour 측에서 Timer > 0인 Entity의 체력바를 렌더링.
+    /// </summary>
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateAfter(typeof(EnemyCombatSystem))]
+    public partial struct HealthBarSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EnemyTag>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            foreach (var (stats, healthBar) in
+                SystemAPI.Query<RefRO<EnemyStats>, RefRW<HealthBarTimer>>()
+                    .WithAll<EnemyTag>()
+                    .WithNone<DeadTag>())
+            {
+                // 체력이 줄었으면 타이머 리셋 (CurrentHP < MaxHP)
+                if (stats.ValueRO.CurrentHP < stats.ValueRO.MaxHP && healthBar.ValueRO.RemainingTime <= 0f)
+                {
+                    healthBar.ValueRW.RemainingTime = 2f;
+                }
+
+                // 타이머 감소
+                if (healthBar.ValueRO.RemainingTime > 0f)
+                {
+                    healthBar.ValueRW.RemainingTime -= deltaTime;
+                }
+            }
+        }
+    }
+}
